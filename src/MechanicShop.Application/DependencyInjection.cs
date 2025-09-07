2292a46137b19f57b0e5c3679c78766eec39c521
Marko Medhat <85482481+Zer0bytte@users.noreply.github.com>
2025-09-07 19:44:38 +0300

    test-ci-cd

--- src/MechanicShop.Application/DependencyInjection.cs
+++ src/MechanicShop.Application/DependencyInjection.cs
@@ -10,6 +10,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        System.Console.WriteLine("Hellp");
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         services.AddMediatR(cfg =>
